27d7c1b194e62648d20802f0082f400ecaf5a6a9
zhaokaifeng <zkfdyx@outlook.com>
2025-07-22 22:56:36 +0800

    zhaokaifeng.com.cn update

--- site/static/js/baidu.js
+++ site/static/js/baidu.js
@@ -1,9 +1,7 @@
-<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?05f2b1e3e50ed0af5e609c2be021e981";
+  hm.src = "https://hm.baidu.com/hm.js?f25960f2fc70070026423aaa8b898d1e";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
